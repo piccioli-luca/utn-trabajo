66da5ca430a8b5302ab2b30fb5bcb7bac799cce2
carlarctg <53100513+carlarctg@users.noreply.github.com>
2025-02-23 19:55:24 -0300

    Adds Static Page

--- src/pages/static.jsx
+++ src/pages/static.jsx
@@ -1,7 +1,17 @@
 import './static.css'
 
 function StaticPage () {
-    return <div>Test</div>
+    return <div>
+        <header>
+            <p>GatosLindos.com</p>
+            <div className='headerButtons'>
+                <button>Ver Gatos</button>
+                <button>Contactar</button>
+            </div>
+        </header>
+        <h1>Bienvenidos a GatosLindos.com!</h1>
+        <h2>Colectamos gatos lindos mediante APIs sobre todo el internet.</h2>
+    </div>
 }
 
 export default StaticPage
