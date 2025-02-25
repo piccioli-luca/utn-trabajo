2665662d76c7fd3270a4b4780064278e356a021e
carlarctg <53100513+carlarctg@users.noreply.github.com>
2025-02-25 19:43:44 -0300

    Basic API function

--- src/pages/api/api.jsx
+++ src/pages/api/api.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
 import { Link } from '../../components/Link.jsx'
 import './api.css'
 
 function ApiPage () {
+    const [catImage, setCatImage] = useState("https://avatars.githubusercontent.com/u/200429001?s=48&v=4")
+    
+    const FetchCatImage = async () => {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random")
+        const data = await response.json()
+        const RandomCatImage = data.message
+        console.log(RandomCatImage)
+        setCatImage(RandomCatImage)
+        // return RandomCatImage
+    }
+    
+    useEffect(() => {
+        FetchCatImage()
+    }, []
+)
+
     return <div>
         <header>
             <p>GatosLindos.com</p>
@@ -12,6 +29,7 @@ function ApiPage () {
             </div>
         </header>
         <h1>Gatos surtidos:</h1>
+        <img src={catImage}/>
     </div>
 }
 
