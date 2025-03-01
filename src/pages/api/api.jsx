ff2a092a1b4d5c50cd44f0d72ee63a2899fc2345
carlarctg <53100513+carlarctg@users.noreply.github.com>
2025-03-01 20:25:43 -0300

    Splits FetchAPI into sub functions

--- src/pages/api/api.jsx
+++ src/pages/api/api.jsx
@@ -4,25 +4,46 @@ import './api.css'
 
 function ApiPage () {
     const [catImage, setCatImage] = useState("https://avatars.githubusercontent.com/u/200429001?s=48&v=4")
+    const [catFact, setCatFact] = useState("Los gatos son muy lindos.")
     
-    const FetchAPI = async (apiLink, setter) => {
-        const response = await fetch(apiLink)
+    const FetchURL = async (setter, response) => {
+        const data = await response.blob() // Convert image response to blob
+        console.log(data)
+        const FetchedURL = URL.createObjectURL(data) // Convert blob to URL
+        return setter(FetchedURL)
+        
+    }
+
+    const FetchText = async (setter, response) => {
         const data = await response.json()
-        const RandomCatImage = data.message
-        console.log(RandomCatImage)
-        setter(RandomCatImage)
-        // return RandomCatImage
+        console.log(data)
+        const FetchedText = data.fact
+        return setter(FetchedText)
+        
     }
     
+    const FetchAPI = async (apiLink, setter, type) => {
+        const response = await fetch(apiLink)
+        return type == 'URL' ? FetchURL(setter, response) : FetchText(setter, response)
+    }
+    
+    
+    
     useEffect(() => {
+        console.log("Calling FetchAPI")
         // Grabs an image of a 'cat'.
-        FetchAPI("https://cataas.com/cat", setCatImage)
+        console.log("Fetching fact...")
+        FetchAPI("https://catfact.ninja/fact", setCatFact, 'fact')
+        console.log("Fetching image...")
+        FetchAPI("https://cataas.com/cat", setCatImage, 'URL')
     }, []
 )
 
     return <Layout>
-            <h1>Gatos surtidos:</h1>
+            <h2>Gato aleatorio:</h2>
             <img src={catImage}/>
+            <h2>Informacion de los ingleses:</h2>
+            <h4>{catFact}</h4>
         </Layout>
 }
 
